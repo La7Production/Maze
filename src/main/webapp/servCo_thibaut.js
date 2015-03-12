a28524a209098459b192f5ea1444db90f7f0d227
Thibaut Jourdain <thibautjourdain@orange.fr>
2015-03-12 16:34:23 +0100

    ssds

--- src/main/webapp/servCo_thibaut.js
+++ src/main/webapp/servCo_thibaut.js
@@ -1,3 +1,29 @@
+/* Une fois les objets json créés selon les touches clavier,
+on utilise ws.send(String) pour l'envoyer au serveur.
+String = JSON.stringify(monobjetjson)
+*/
+
+var ws = new WebSocket("ws://127.0.0.1:9876/");
+
+ws.onopen = function() {
+	alert("Opened!");
+	ws.send("Hello Server i'm a client");
+};
+
+ws.onmessage = function (evt) {
+	console.log(JSON.parse(evt.data));
+};
+
+ws.onclose = function() {
+	alert("Closed!");
+};
+
+ws.onerror = function(err) {
+	alert("Error: " + err);
+};
+
+
+
 function hideMenuCrea() {
 	$('#creerCompte').hide();
 }
@@ -14,7 +40,25 @@ function showMenuCo() {
 	$('#connexion').show();
 }
 
-function creaCompte() {
+function paintCanvas(){
+	var c = document.getElementById("exemple");
+	var ctx = c.getContext("2d");
+	
+	for(var i = 0; i < 5; i++){
+		for(var j = 0; j < 5; j++){
+			if((i+j)%2 === 0) {
+				ctx.fillStyle = "rgba(0,0,128,128)";
+   			ctx.fillRect(i*100, j*100, 100, 100);
+			} else {
+				ctx.fillStyle = "rgba(0,0,255,0.5)";
+   			ctx.fillRect(i*100, 100*j, 100, 100);
+			}
+					
+		}		    	
+	}
+}
+
+function creaCompte(){
 	$('#creer').click(function(event) {
 			$.ajax({
 			 
@@ -55,25 +99,46 @@ function coCompte() {
 	});
 }
 
-function deplacementJoueur(login) {
+function deplacementJoueur(login, event) {
+	var envoi;
+	if(event.keyCode===37){
+		envoi={
+				login:login,
+				direction:"ouest",
+			};
+	} else if(event.keyCode===38){
+		envoi={
+				login:login,
+				direction:"nord",
+			};
+	} else if(event.keyCode===39){
+		envoi={
+				login:login,
+				direction:"est",
+			};
+	} else if(event.keyCode===40){
+		envoi={
+				login:login,
+				direction:"sud",
+			};
+	}	
 	
+	console.log(envoi);
+	/*ws.send(JSON.stringify(envoi));*/
+	
+	/*$.ajax({
+				url: "url du serveur",
+				type: "PUT"	,
+				dataType: "json",
+				
+				data : envoi,
+				
+				success: function (json) {
+					alert("Appuie sur la touche :" + json);
+				},
+						
+			});*/
 }
 
-function paintCanvas(){
-	var c = document.getElementById("exemple");
-	var ctx = c.getContext("2d");
-	
-	for(var i = 0; i < 5; i++){
-		for(var j = 0; j < 5; j++){
-			if((i+j)%2 === 0) {
-				ctx.fillStyle = "rgba(0,0,128,128)";
-   			ctx.fillRect(i*100, j*100, 100, 100);
-			} else {
-				ctx.fillStyle = "rgba(0,0,255,0.5)";
-   			ctx.fillRect(i*100, 100*j, 100, 100);
-			}
-					
-		   }		    	
-		}
-	}
+
 
