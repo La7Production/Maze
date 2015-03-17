88f3ff1308d0e37d399b7e51b83e50228789224f
Javadoc <teamexcalicraft@gmail.com>
2015-03-17 19:01:29 +0100

    Backup tests JS et préparation au Client JS

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,70 @@
+var playername;
+var data;
+var ws;
+
+/*************************
+ * PARTIE REST VIA AJAX
+ *************************/
+
+/* Crée un nouvel utilisateur dans le serveur */
+function signin(login, password, firstname, lastname, birthday, email) {
+
+};
+
+/* Identifie un utilisateur dans le serveur et en ressort un joueur */
+playername = function signup(login, password) {
+
+};
+
+/*************************
+ * PARTIE WEBSOCKET
+ *************************/
+
+/* Fonction appelée à l'ouverture d'une websocket */
+ws.onopen = function() {
+	
+};
+
+/* Fonction appelée lorsque des données sont envoyées du serveur au client */
+ws.onmessage = function (evt) {
+	
+};
+
+/* Fonction appelée lorsque la websocket est fermée */
+ws.onclose = function() {
+	
+};
+
+/* Fonction appelée en cas d'erreur dans la websocket */
+ws.onerror = function(err) {
+	
+};
+
+/*************************
+ * PARTIE GRAPHIQUE
+ *************************/
+
+/* Fonction appelée lorsque l'utilisateur appuie sur des touches précises de son clavier */
+function actionPerformed() {
+
+};
+
+/* Fonction appelée lorsque l'utilisateur clique avec sa souris */
+function mouseClicked() {
+
+};
+
+/* Affiche un message d'attente lorsque le nombre minimal de joueurs requis dans une partie n'est pas atteint */
+function waitPlayers() {
+
+};
+
+/* Dessine le labyrinthe vide en considérant la variable data comme définie (data.maze.cells)*/
+function drawMaze() {
+
+};
+
+/* Dessine les joueurs dans le labyrinthe */
+function drawPlayers() {
+
+};
